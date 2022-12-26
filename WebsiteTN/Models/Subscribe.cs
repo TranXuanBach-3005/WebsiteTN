dea4c44f45dea8e4c42e6a1f1812d57120445319
XuanBach3005 <115221386+XuanBach3005@users.noreply.github.com>
2022-12-26 15:18:44 +0700

    add post

---
+++ WebsiteTN/Models/Subscribe.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteTN.Models
+{
+    public class Subscribe
+    {
+        public int SubscribeId { get; set; }
+        public string Email { get; set; }
+        public DateTime CreateDate { get; set; }
+    }
+}
