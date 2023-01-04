09d90ccb8628ee17687aa098725b03c1c4d09f6d
XuanBach3005 <115221386+XuanBach3005@users.noreply.github.com>
2023-01-04 17:11:05 +0700

    first cart

---
+++ WebsiteTN/Controllers/LocationControllers.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebsiteTN.Models;
+
+namespace WebsiteTN.Controllers
+{
+    public class LocationControllers : Controller
+    {
+        private readonly DataDbContext _context;
+
+        public LocationControllers(DataDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ListDistrict(int LocationId)
+        {
+            var Districts = _context.Locations.OrderBy(x => x.LocationId)
+                                              .Where(x => x.Parent == LocationId && x.Levels == 2)
+                                              .OrderBy(x => x.NameWithType)
+                                              .ToList();
+            return Json(Districts);
+        }
+        public IActionResult ListWard(int LocationId)
+        {
+            var Wards = _context.Locations.OrderBy(x => x.LocationId)
+                                              .Where(x => x.Parent == LocationId && x.Levels == 3)
+                                              .OrderBy(x => x.NameWithType)
+                                              .ToList();
+            return Json(Wards);
+        }
+    }
+}
---
+++ WebsiteTN/wwwroot/clientassets/cartjs/indexCart.js
@@ -0,0 +1,46 @@
+﻿$(function () {
+    function loadHeaderCart() {
+        $('#miniCart').load("/AjaxSoucre/HeaderCart");
+        $('#numberCart').load("/AjaxSoucer/NumberCart");
+    }
+    $(".removerCart").click(function () {
+        var productId = $(this).attr("data-id");
+        $.ajax({
+            url: "api/cart/remove",
+            type: "POST",
+            dataType: "JSON",
+            data: { productId: productId },
+            success: function (result) {
+                if (result.success) {
+                    loadHeaderCart();
+                    location.reload();
+                }
+            },
+            error: function (rs) {
+                alert("Remove Cart Error");
+            }
+        });
+    });
+    $(".cartItem").click(function () {
+        var productId = $(this).attr("data-id");
+        var quantity = parseInt($(this).val());
+        $.ajax({
+            url: "api/cart/update",
+            type: "POST",
+            dataType: "JSON",
+            data: {
+                productId: productId,
+                quantity: quantity
+            },
+            success: function (result) {
+                if (result.success) {
+                    loadHeaderCart();
+                    window.location = 'cart.html';
+                }
+            },
+            error: function (rs) {
+                alert("Update Cart Error");
+            }
+        });
+    });
+});
